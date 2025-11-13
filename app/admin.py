fb5586f5a7e6dbe9c9bf7ba1e2a3324b7e209547
ArturDaniel123 <artur.daniel.borges2@hotmail.com>
2025-11-13 12:28:44 -0300

    Criado o serializers.py

--- app/admin.py
+++ app/admin.py
@@ -3,7 +3,10 @@ from app.models import Cardapio
 
 
 class Cardapios(admin.ModelAdmin):
-    list_display = ('id', 'prato', 'preco', 'estoque', 'disponibilidade')
-    list_display_links =
-    list_per_page
-    search_fields
+    list_display = ('id', 'prato', 'preco', 'estoque', 'disponibilidade',)
+    list_display_links = ('id', 'prato',)
+    list_per_page = 20
+    search_fields = ('prato',)
+
+
+admin.site.register(Cardapio, Cardapios)
