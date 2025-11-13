a7e3a3e719133c64ceb16a15d9f1cfe7a525f35a
ArturDaniel123 <artur.daniel.borges2@hotmail.com>
2025-11-13 16:46:13 -0300

    Adicionei login e registro na API

--- app/views.py
+++ app/views.py
@@ -1,8 +1,12 @@
 from app.models import Cardapio
 from app.serializers import CardapioSerializer
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from django.contrib.auth.models import User
 from app.serializers import RegistroSerializer
+from rest_framework.response import Response
+from django.contrib.auth import authenticate
+from rest_framework.views import APIView
+from rest_framework.decorators import action
 
 
 class CardapioViewSet(viewsets.ModelViewSet):
@@ -13,3 +17,28 @@ class CardapioViewSet(viewsets.ModelViewSet):
 class RegistroViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = RegistroSerializer
+
+    @action(detail=False, methods=['post'], url_path='login')
+    def login(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            return Response({'mensagem': 'Login efetuado com sucesso!'}, status=status.HTTP_200_OK)
+        else:
+            return Response({'erro': 'Credenciais inválidas.'}, status=status.HTTP_401_UNAUTHORIZED)
+
+
+class LoginView(APIView):
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            return Response({'mensagem': 'Login efetuado'}, status=status.HTTP_200_OK)
+        else:
+            return Response({'erro': 'Credenciais inválidas.'}, status=status.HTTP_401_UNAUTHORIZED)
--- setup/urls.py
+++ setup/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from app.views import CardapioViewSet, RegistroViewSet
+from app.views import CardapioViewSet, RegistroViewSet, LoginView
 from rest_framework import routers
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework.routers import DefaultRouter
 
 router = routers.DefaultRouter()
 router.register('cardapio', CardapioViewSet, basename='Cardapio')
@@ -10,5 +12,8 @@ router.register('registro', RegistroViewSet, basename='Registro')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('login/', LoginView.as_view(), name='login'),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
